84d9a38698cdd416f3381eae5c298c471448b8d1
Aaron J Rubin <aaronjrubin1@gmail.com>
2014-09-15 00:00:21 +0900

    working prototype

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 #
 # Copyright 2007 Google Inc.
 #
@@ -17,9 +18,28 @@
 import os
 import webapp2
 import jinja2
+import re
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
+
+def render_str(template, **params):
+    t = jinja_env.get_template(template)
+    return t.render(params)
+
+class FishTile:
+
+    def __init__(self, label = "さけ", link = "sakana/sake", image = "static/sake.jpg"):
+        self.label = label.decode('utf-8');
+        self.link = link.decode('utf-8');
+        if not image.startswith("static/"):
+            image = "static/" + image
+        self.image = image 
+
+    def render(self):
+        return render_str("fish-tile.html", fish = self)
+
+fish_tiles = [FishTile() for _ in range(80)]
 
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
@@ -30,12 +50,18 @@ class Handler(webapp2.RequestHandler):
         return t.render(**params)
 
     def render(self, template, **params):
-        self.write(self.render_str(template, **params))
+        self.write(self.render_str(template, **params).encode('utf-8'))
 
 class MainHandler(Handler):
     def get(self):
-        self.render("index.html");
+        self.render("sakana-ichiran.html", fish_tiles = fish_tiles);
 
+class FishPageHandler(Handler):
+    def get(self, FISH_RE):
+        print("Calling FishPageHandler")
+        self.render("sakana" + FISH_RE + ".html")
+
+FISH_RE = r'(/(?:[a-zA-Z0-9_-]+/?)*)'
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler), ('/sakana' + FISH_RE, FishPageHandler)
 ], debug=True)
