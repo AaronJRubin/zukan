ef82dd86745124b4c0cc2b16e798e85e68c462dc
Aaron J Rubin <aaronjrubin1@gmail.com>
2015-01-12 21:52:10 +0900

    Removed broken and unnecessary import

--- zukan-site/main.py
+++ zukan-site/main.py
@@ -19,7 +19,6 @@ import os
 import webapp2
 import jinja2
 import re
-from romaji import kana
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
