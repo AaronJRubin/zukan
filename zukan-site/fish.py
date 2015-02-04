9030daaf62d59c6ad758a78205897cf4b1e59992
Aaron J Rubin <aaronjrubin1@gmail.com>
2015-02-04 19:48:40 +0900

    Successful fish class refactoring

---
+++ zukan-site/fish.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import jinja_utils
+
+class Location:
+
+    ZYOU, CHUU, GE, KAKOU = range(4)
+
+    def __init__(self):
+        self._dict = {"Takatsugawa" : {}, "Masuda" : {}}
+        self._dict["Takatsugawa"] = set()
+        self._dict["Masuda"] = set()
+
+    def set_takatsugawa(self, *locations):
+        for location in locations:
+            self._dict["Takatsugawa"].add(location)
+
+    def set_masuda(self, *locations):
+        for location in locations:
+            self._dict["Takatsugawa"].add(location)
+
+    def get_takatsugawa(self):
+        return frozenset(self._dict["Takatugawa"])
+
+    def get_masuda(self):
+        return frozenset(self._dict["Masuda"])
+
+class Fish:
+
+    def __init__(self, latin = "piscis maximus", genus = u"魚科", romaji = "ayu", kana = u"アユ", location = Location()):
+        self.romaji = romaji
+        try:
+            self.kana = kana.decode('utf-8')
+        except Exception:
+            self.kana = kana
+        self.latin = latin
+        self.genus = genus
+        self.location = location
+        self.link = "sakana/" + romaji
+        self.image = "static/low-res/ichiran/" + romaji + ".jpg"
+
+    def render_tile(self):
+        return jinja_utils.render_str("fish-tile.html", fish = self)
+
+akaza_loc = Location()
+akaza_loc.set_takatsugawa(Location.ZYOU)
+akaza_loc.set_masuda(Location.ZYOU)
+akaza = Fish(romaji = "akaza", kana = "アカザ", location = akaza_loc)
+
+fish_list = [Fish(romaji = "abehaze", kana = "アベハゼ"),
+Fish(romaji = "aburabote", kana = "アブラボテ"),
+Fish(romaji = "akaza", kana = "アカザ"),
+Fish(romaji = "amago", kana = "アマゴ"),
+Fish(romaji = "ayu", kana = "アユ"),
+Fish(romaji = "ayukake", kana = "アユカケ"),
+Fish(romaji = "bora", kana = "ボラ"),
+Fish(romaji = "dojou", kana = "ドジョウ"),
+Fish(romaji = "donnko", kana = "ドンコ"),
+Fish(romaji = "gigi", kana = "ギギ"),
+Fish(romaji = "gogi", kana = "ゴギ"),
+Fish(romaji = "gokurakuhaze", kana = "ゴクラクハゼ"),
+Fish(romaji = "himehaze", kana = "ヒメハゼ"),
+Fish(romaji = "hinahaze", kana = "ヒナハゼ"),
+Fish(romaji = "hirasuzuki", kana = "ヒラスズキ"),
+Fish(romaji = "huna", kana = "フナ"),
+Fish(romaji = "ishidojou", kana = "イシドジョウ"),
+Fish(romaji = "ishidonko", kana = "イシドンコ"),
+Fish(romaji = "itomoroko", kana = "イトモロコ"),
+Fish(romaji = "kajika", kana = "カジカ"),
+Fish(romaji = "kamatuka", kana = "カマツカ"),
+Fish(romaji = "kawaanago", kana = "カワアナゴ"),
+Fish(romaji = "kawamutu", kana = "カワムツ"),
+Fish(romaji = "kawayoshinobori", kana = "カワヨシノボリ"),
+Fish(romaji = "koi", kana = "コイ"),
+Fish(romaji = "kurodai", kana = "クロダイ"),
+Fish(romaji = "magoti", kana = "マゴチ"),
+Fish(romaji = "mahaze", kana = "マハゼ"),
+Fish(romaji = "medaka", kana = "メダカ"),
+Fish(romaji = "mimizuhaze", kana = "ミミズハゼ"),
+Fish(romaji = "mugituku", kana = "ムギツク"),
+Fish(romaji = "namazu", kana = "ナマズ"),
+Fish(romaji = "numatitibu", kana = "ヌマチチブ"),
+Fish(romaji = "oikawa", kana = "オイカワ"),
+Fish(romaji = "ooyosinobori", kana = "オオヨシノボリ"),
+Fish(romaji = "oyanirami", kana = "オヤニラミ"),
+Fish(romaji = "ribbinngo", kana = "リッビンゴ"),
+Fish(romaji = "sakuramasu", kana = "サクラマス"),
+Fish(romaji = "shimadojou", kana = "シマドジョウ"),
+Fish(romaji = "shimaisaki", kana = "シマイサキ"),
+Fish(romaji = "shirogisu", kana = "シロギス"),
+Fish(romaji = "shirouo", kana = "シロウオ"),
+Fish(romaji = "sumiukigori", kana = "スミウキゴリ"),
+Fish(romaji = "sunayatume", kana = "スナヤツメ"),
+Fish(romaji = "suzuki", kana = "スズキ"),
+Fish(romaji = "tairikubaratanago", kana = "タイリクバラタナゴ"),
+Fish(romaji = "titibu", kana = "チチブ"),
+Fish(romaji = "touyoshinobori", kana = "トウヨシノボリ"),
+Fish(romaji = "ugui", kana = "ウグイ"),
+Fish(romaji = "ukigori", kana = "ウキゴリ"),
+Fish(romaji = "unagi", kana = "ウナギ"),
+Fish(romaji = "urohaze", kana = "ウロハゼ"),
+Fish(romaji = "yamame", kana = "ヤマメ")]
+
+fish_dict = {}
+
+for a_fish in fish_list:
+    fish_dict[a_fish.romaji] = a_fish
+
+fish_dict["akaza"] = akaza
--- zukan-site/main.py
+++ zukan-site/main.py
@@ -19,133 +19,24 @@ import os
 import webapp2
 import jinja2
 import re
-
-template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
-
-def render_str(template, **params):
-    t = jinja_env.get_template(template)
-    return t.render(params)
-
-class FishTile:
-
-    def __init__(self, romaji = "ayu", label = "アユ"):
-        #if label == None:
-        #    label = romkan.to_katakana(romaji)
-        self.label = label.decode('utf-8');
-        self.link = "sakana/" + romaji
-        self.image = "static/low-res/ichiran/" + romaji + ".jpg"
-
-    def render(self):
-        return render_str("fish-tile.html", fish = self)
-
-class Location:
-
-    ZYOU, CHUU, GE, KAKOU = range(4)
-
-    def __init__(self):
-        self._dict = {"Takatsugawa" : {}, "Masuda" : {}}
-        self._dict["Takatsugawa"] = set()
-        self._dict["Masuda"] = set()
-
-    def set_takatsugawa(locations):
-        for location in locations:
-            self._dict["Takatsugawa"].add(location)
-
-    def set_masuda(locations):
-        for location in locations:
-            self._dict["Takatsugawa"].add(location)
-
-    def get_takatsugawa():
-        return frozenset(self._dict["Takatugawa"])
-
-    def get_masuda():
-        return frozenset(self._dict["Masuda"])
-
-class Fish:
-
-    def __init__(self, latin = "piscis maximus", genus = u"魚科", romaji = "ayu", kana = u"アユ", location = u"高津川"):
-        self.romaji = romaji
-        self.kana = kana
-        self.latin = latin
-        self.genus = genus
-        self.location = location
-
-fish_tiles = [FishTile(romaji = "abehaze", label = "アベハゼ"),
-FishTile(romaji = "aburabote", label = "アブラボテ"),
-FishTile(romaji = "akaza", label = "アカザ"),
-FishTile(romaji = "amago", label = "アマゴ"),
-FishTile(romaji = "ayu", label = "アユ"),
-FishTile(romaji = "ayukake", label = "アユカケ"),
-FishTile(romaji = "bora", label = "ボラ"),
-FishTile(romaji = "dojou", label = "ドジョウ"),
-FishTile(romaji = "donnko", label = "ドンコ"),
-FishTile(romaji = "gigi", label = "ギギ"),
-FishTile(romaji = "gogi", label = "ゴギ"),
-FishTile(romaji = "gokurakuhaze", label = "ゴクラクハゼ"),
-FishTile(romaji = "himehaze", label = "ヒメハゼ"),
-FishTile(romaji = "hinahaze", label = "ヒナハゼ"),
-FishTile(romaji = "hirasuzuki", label = "ヒラスズキ"),
-FishTile(romaji = "huna", label = "フナ"),
-FishTile(romaji = "ishidojou", label = "イシドジョウ"),
-FishTile(romaji = "ishidonko", label = "イシドンコ"),
-FishTile(romaji = "itomoroko", label = "イトモロコ"),
-FishTile(romaji = "kajika", label = "カジカ"),
-FishTile(romaji = "kamatuka", label = "カマツカ"),
-FishTile(romaji = "kawaanago", label = "カワアナゴ"),
-FishTile(romaji = "kawamutu", label = "カワムツ"),
-FishTile(romaji = "kawayoshinobori", label = "カワヨシノボリ"),
-FishTile(romaji = "koi", label = "コイ"),
-FishTile(romaji = "kurodai", label = "クロダイ"),
-FishTile(romaji = "magoti", label = "マゴチ"),
-FishTile(romaji = "mahaze", label = "マハゼ"),
-FishTile(romaji = "medaka", label = "メダカ"),
-FishTile(romaji = "mimizuhaze", label = "ミミズハゼ"),
-FishTile(romaji = "mugituku", label = "ムギツク"),
-FishTile(romaji = "namazu", label = "ナマズ"),
-FishTile(romaji = "numatitibu", label = "ヌマチチブ"),
-FishTile(romaji = "oikawa", label = "オイカワ"),
-FishTile(romaji = "ooyosinobori", label = "オオヨシノボリ"),
-FishTile(romaji = "oyanirami", label = "オヤニラミ"),
-FishTile(romaji = "ribbinngo", label = "リッビンゴ"),
-FishTile(romaji = "sakuramasu", label = "サクラマス"),
-FishTile(romaji = "shimadojou", label = "シマドジョウ"),
-FishTile(romaji = "shimaisaki", label = "シマイサキ"),
-FishTile(romaji = "shirogisu", label = "シロギス"),
-FishTile(romaji = "shirouo", label = "シロウオ"),
-FishTile(romaji = "sumiukigori", label = "スミウキゴリ"),
-FishTile(romaji = "sunayatume", label = "スナヤツメ"),
-FishTile(romaji = "suzuki", label = "スズキ"),
-FishTile(romaji = "tairikubaratanago", label = "タイリクバラタナゴ"),
-FishTile(romaji = "titibu", label = "チチブ"),
-FishTile(romaji = "touyoshinobori", label = "トウヨシノボリ"),
-FishTile(romaji = "ugui", label = "ウグイ"),
-FishTile(romaji = "ukigori", label = "ウキゴリ"),
-FishTile(romaji = "unagi", label = "ウナギ"),
-FishTile(romaji = "urohaze", label = "ウロハゼ"),
-FishTile(romaji = "yamame", label = "ヤマメ")]
-
-#fish_tiles = [FishTile() for _ in range(80)]
+import fish
+import jinja_utils
 
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
 
-    def render_str(self, template, **params):
-        t = jinja_env.get_template(template)
-        return t.render(**params)
-
     def render(self, template, **params):
-        self.write(self.render_str(template, **params).encode('utf-8'))
+        self.write(jinja_utils.render_str(template, **params).encode('utf-8'))
 
 class MainHandler(Handler):
     def get(self):
-        self.render("sakana-ichiran.html", fish_tiles = fish_tiles);
+        self.render("sakana-ichiran.html", fish = fish.fish_dict.values());
 
 class FishPageHandler(Handler):
     def get(self, FISH_RE):
         print("Calling FishPageHandler")
-        self.render("sakana" + FISH_RE + ".html", fish = Fish()) # later on look up the actual fish
+        self.render("sakana" + FISH_RE + ".html", fish = fish.fish_dict[FISH_RE[1:]])
 
 FISH_RE = r'(/(?:[a-zA-Z0-9_-]+/?)*)'
 app = webapp2.WSGIApplication([
