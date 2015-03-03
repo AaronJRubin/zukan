ca4ebf4931b60c2168c352d88487c35c16e57f76
Aaron J Rubin <aaronjrubin1@gmail.com>
2015-03-03 12:26:55 +0900

    Working slideshow functionality

--- static_site/web/fish.dart
+++ static_site/web/fish.dart
@@ -1,6 +1,6 @@
 library fish;
 
-part '../fish_list.dart';
+part 'fish_list.dart';
 
 abstract class RiverHabitat {
 
@@ -14,16 +14,16 @@ abstract class RiverHabitat {
   List<String> backgroundImages() {
     List<String> res = [];
       if (up) {
-        res.add(_constructURL("up"));
+        res.add(_constructURL("zyou"));
       }
       if (central) {
-          res.add(_constructURL("central"));
+          res.add(_constructURL("chuu"));
        }
       if (down) {
-        res.add(_constructURL("down"));
+        res.add(_constructURL("ge"));
       }
       if (mouth) {
-        res.add(_constructURL("mouth"));
+        res.add(_constructURL("kakou"));
       }
       return res;
   }
@@ -39,7 +39,7 @@ class Takatsu extends RiverHabitat {
   }
 
   String _constructURL(String area) {
-    return "takatsu-" + area + ".jpg";
+    return "/seisokuchi/takatsu-" + area + ".jpg";
   }
 
 }
@@ -54,7 +54,7 @@ class Masuda extends RiverHabitat {
   }
 
   String _constructURL(String area) {
-    return "masuda-" + area + ".jpg";
+    return "/seisokuchi/masuda-" + area + ".jpg";
   }
 
 }
@@ -72,6 +72,12 @@ class Fish {
 
   Fish(this.scientific, this.family, this.genus, this.romaji, this.kana, this.rarity,
       bool takatsu_up, bool takatsu_central, bool takatsu_down, bool takatsu_mouth,
-      bool masuda_up, bool masuda_central, bool masuda_down, bool masuda_mouth);
+      bool masuda_up, bool masuda_central, bool masuda_down, bool masuda_mouth) {
+    this.takatsu = new Takatsu(takatsu_up, takatsu_central, takatsu_down, takatsu_mouth);
+    this.masuda = new Masuda(masuda_up, masuda_central, masuda_down, masuda_mouth);
+  }
 
+  List<String> backgroundImages() {
+    return takatsu.backgroundImages()..addAll(masuda.backgroundImages());
+  }
 }
--- static_site/web/sakana/fish_page.dart
+++ static_site/web/sakana/fish_page.dart
@@ -1,7 +1,36 @@
 import 'dart:html';
+import '../fish.dart';
+import 'dart:async';
+
+BodyElement body = document.querySelector("body");
+List<String> images = getBackgroundImages();
+int currentImage = 0;
 
 main() {
-  print("Hello from dart!");
-  String fish_name = document.baseUri.split("/").last.replaceFirst(".html", "");
-  print("My name is " + fish_name);
+  if (window.screen.width > 700) {
+    window.onResize.listen((e) {
+      if (window.innerWidth < 700) {
+        body.style.backgroundImage = "none";
+      } else {
+        body.style.backgroundImage = "url(" + images[currentImage] + ")";
+      }
+    });
+    Timer timer = new Timer.periodic(new Duration(seconds: 5), (t) {
+      if (window.innerWidth >= 700) {
+      body.style.backgroundImage = "url(" + images[currentImage] + ")";
+      }
+      currentImage += 1;
+      if (currentImage >= images.length) {
+        currentImage = 0;
+      }
+    });
+  }
 }
+
+
+List<String> getBackgroundImages() {
+  String fish_name = document.baseUri.split("/").last.replaceFirst(".html", "");
+  Fish fish = fish_map[fish_name];
+  return fish.backgroundImages();
+}
+
