88e7885c6de1494c5b36c7b25d60d37e40f312db
Aaron J Rubin <aaronjrubin1@gmail.com>
2015-03-05 15:28:41 +0900

    Finish refactoring fish.dart and fish_page.dart, small CSS change for padding on small screens

--- zukan_workspace/web/fish.dart
+++ zukan_workspace/web/fish.dart
@@ -2,61 +2,37 @@ library fish;
 
 part 'fish_list.dart';
 
-abstract class RiverHabitat {
+class RiverHabitat {
 
   bool up;
   bool central;
   bool down;
   bool mouth;
+  String name;
 
-  String _constructURL(String area);
+  RiverHabitat(this.up, this.central, this.down, this.mouth, this.name) {
+    this.up = up;
+    this.central = central;
+    this.down = down;
+    this.mouth = mouth;
+  }
 
-  List<String> backgroundImages() {
+  List<String> backgroundImageClasses() {
     List<String> res = [];
-      if (up) {
-        res.add(_constructURL("zyou"));
-      }
-      if (central) {
-          res.add(_constructURL("chuu"));
-       }
-      if (down) {
-        res.add(_constructURL("ge"));
-      }
-      if (mouth) {
-        res.add(_constructURL("kakou"));
-      }
-      return res;
+    if (up) {
+      res.add(name + "-zyou");
+    }
+    if (central) {
+      res.add(name + "-chuu");
+    }
+    if (down) {
+      res.add(name + "-ge");
+    }
+    if (mouth) {
+      res.add(name + "-kakou");
+    }
+    return res;
   }
-}
-
-class Takatsu extends RiverHabitat {
-
-  Takatsu(bool up, bool central, bool down, bool mouth) {
-   this.up = up;
-   this.central = central;
-   this.down = down;
-   this.mouth = mouth;
-  }
-
-  String _constructURL(String area) {
-    return "/seisokuchi/takatsu-" + area + ".jpg";
-  }
-
-}
-
-class Masuda extends RiverHabitat {
-
-  Masuda(bool up, bool central, bool down, bool mouth) {
-   this.up = up;
-   this.central = central;
-   this.down = down;
-   this.mouth = mouth;
-  }
-
-  String _constructURL(String area) {
-    return "/seisokuchi/masuda-" + area + ".jpg";
-  }
-
 }
 
 class Fish {
@@ -70,14 +46,13 @@ class Fish {
   RiverHabitat takatsu;
   RiverHabitat masuda;
 
-  Fish(this.scientific, this.family, this.genus, this.romaji, this.kana, this.rarity,
-      bool takatsu_up, bool takatsu_central, bool takatsu_down, bool takatsu_mouth,
-      bool masuda_up, bool masuda_central, bool masuda_down, bool masuda_mouth) {
-    this.takatsu = new Takatsu(takatsu_up, takatsu_central, takatsu_down, takatsu_mouth);
-    this.masuda = new Masuda(masuda_up, masuda_central, masuda_down, masuda_mouth);
+  Fish(this.scientific, this.family, this.genus, this.romaji, this.kana, this.rarity, bool takatsu_up, bool takatsu_central, bool takatsu_down, bool takatsu_mouth, bool masuda_up, bool masuda_central, bool masuda_down, bool masuda_mouth) {
+    this.takatsu = new RiverHabitat(takatsu_up, takatsu_central, takatsu_down, takatsu_mouth, "takatsu");
+    this.masuda = new RiverHabitat(masuda_up, masuda_central, masuda_down, masuda_mouth, "masuda");
   }
 
-  List<String> backgroundImages() {
-    return takatsu.backgroundImages()..addAll(masuda.backgroundImages());
+  List<String> backgroundImageClasses() {
+    return takatsu.backgroundImageClasses()..addAll(masuda.backgroundImageClasses());
   }
+
 }
--- zukan_workspace/web/sakana/fish_page.dart
+++ zukan_workspace/web/sakana/fish_page.dart
@@ -3,36 +3,22 @@ import '../fish.dart';
 import 'dart:async';
 
 BodyElement body = document.querySelector("body");
-List<String> images = getBackgroundImages();
-int currentImage = 1;
+List<String> imageClasses = getBackgroundImages();
+int currentImageClass = 0;
 
 main() {
-  if (window.screen.width > 700 && images.length > 1) {
-    window.onResize.listen((e) {
-      if (window.innerWidth < 700) {
-        body.style.backgroundImage = "none";
-      } else {
-        body.style.backgroundImage = "url(" + images[currentImage] + ")";
-      }
-    });
+  if (window.screen.width > 700 && imageClasses.length > 1) {
     Timer timer = new Timer.periodic(new Duration(seconds: 15), (t) {
-      if (window.innerWidth >= 700) {
-      body.style.backgroundImage = "url(" + images[currentImage] + ")";
-      }
-      currentImage += 1;
-      if (currentImage >= images.length) {
-        currentImage = 0;
-      }
+      body.classes.remove(imageClasses[currentImageClass]);
+      currentImageClass = (currentImageClass + 1) % imageClasses.length;
+      body.classes.add(imageClasses[currentImageClass]);
     });
   }
 }
 
-
 List<String> getBackgroundImages() {
   String fish_name = document.baseUri.split("/").last.replaceFirst(".html", "");
-  print("Name: " + fish_name);
   Fish fish = fish_map[fish_name];
-  print("Kana: " + fish.kana);
-  return fish.backgroundImages();
+  return fish.backgroundImageClasses();
 }
 
