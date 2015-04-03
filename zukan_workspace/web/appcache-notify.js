41f3a80c5a637330e339e9c793e3754cd99cada8
Aaron J Rubin <aaronjrubin1@gmail.com>
2015-04-03 11:14:23 +0900

    Use total attribute of progress event to determine what to show in the progress bar, rather than hardcoding in a number

--- zukan_workspace/web/appcache-notify.js
+++ zukan_workspace/web/appcache-notify.js
@@ -2,13 +2,17 @@ var appcache = window.applicationCache;
 
 appcache.addEventListener("downloading", function(event) {
 		var notifications = document.querySelector("#appcache-notifications");	
-		notifications.innerHTML = 'キャッシュ更新中！しばらくこのページに残ってください。<span id="downloaded">0</span>/244ダウンロード済み。';
+		notifications.innerHTML = 'キャッシュ更新中！しばらくこのページに残ってください。<span id="downloaded">0</span>/<span id="total">244</span>ダウンロード済み。';
 		notifications.classList.toggle("displayed");
-		var filesDownloaded = 0;
+		var currentTotal = 244;
 		var downloaded = document.querySelector("#downloaded");
+		var total = document.querySelector("#total");
 		appcache.addEventListener("progress", function(event) {
-			filesDownloaded += 1;
-			downloaded.innerHTML = filesDownloaded;
+			if (currentTotal != event.total) {
+				currentTotal = event.total;
+				total.innerHTML = currentTotal;
+			}
+			downloaded.innerHTML = event.loaded;
 			});
 		updated = function(event) {
 		notifications.innerHTML = "キャッシュされました！変わったサイトを見るためにこのページをリフレッシュしてください。";
