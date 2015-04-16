1be6060d58981411682ec7a792a7faf4e3ae5b79
Aaron J Rubin <aaronjrubin1@gmail.com>
2015-04-16 15:47:22 +0900

    Implement browser detection for backgrochanging background image feature (becausely the background image transition property is implemented only in Chrome and Safari, and only looks good in Chrome, let's only change background images in Chrome)

--- zukan_workspace/web/ikimono/fish_page.dart
+++ zukan_workspace/web/ikimono/fish_page.dart
@@ -1,14 +1,13 @@
 import 'dart:html';
 import '../fish.dart';
 import 'dart:async';
-
-
+import 'package:browser_detect/browser_detect.dart';
 
 main() {
   String fish_name = document.baseUri.split("/").last.replaceFirst(".html", "");
   Fish fish = fish_map[fish_name];
   List<String> imageClasses = fish.backgroundImageClasses();
-  if (window.screen.width > 700 && imageClasses.length > 1) {
+  if (window.screen.width > 700 && imageClasses.length > 1 && browser.isChrome) {
     List<String> backgroundImages = fish.backgroundImages();
     for (String backgroundImage in backgroundImages) {
       new ImageElement(src: backgroundImage); // preload background images
