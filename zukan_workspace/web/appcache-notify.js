0387a11d93e69e890657dfe7dab2e182c4d8885b
Aaron J Rubin <aaronjrubin1@gmail.com>
2015-05-11 15:16:55 +0900

    Modify appcache-notify.js to better follow js conventions, and also to refrain from attempting to display progress in browsers that don't support it (before, such browsers displayed a message that undefined/undefined files have been downloaded, which is very poor UX)

--- zukan_workspace/web/appcache-notify.js
+++ zukan_workspace/web/appcache-notify.js
@@ -1,27 +1,33 @@
 var appcache = window.applicationCache;
-
-appcache.addEventListener("downloading", function(event) {
-    var notifications = document.querySelector("#appcache-notifications");	
-    notifications.innerHTML = 'キャッシュ更新中！しばらくこのページに残ってください。<span id="downloaded">0</span>/<span id="total">350</span>ダウンロード済み。';
+appcache.addEventListener("downloading", function () {
+    "use strict";
+    var notifications = document.querySelector("#appcache-notifications");
+    notifications.innerHTML = 'キャッシュ更新中！しばらくこのページに残ってください。<span id="progress"></span>';
     notifications.classList.toggle("displayed");
-    var currentTotal = 244;
-    var downloaded = document.querySelector("#downloaded");
-    var total = document.querySelector("#total");
-    appcache.addEventListener("progress", function(event) {
-        if (currentTotal != event.total) {
-            currentTotal = event.total;
-            total.innerHTML = currentTotal;
+    var progress = document.querySelector("#progress");
+    var downloaded, total;
+    appcache.addEventListener("progress", function (event) {
+        if (event.total !== 'undefined') {
+            if (downloaded === 'undefined') {
+                progress.innerHTML = '<span id="downloaded">0</span>/<span id="total">349</span>ダウンロード済み。';
+                downloaded = document.querySelector("#downloaded");
+                total = document.querySelector("#total");
+            }
+            downloaded.innerHTML = event.loaded;
+            total.innerHTML = event.total;
         }
-        downloaded.innerHTML = event.loaded;
     });
-    updated = function(event) {
+    var finish = function () {
+        notifications.classList.toggle("finished");
+    };
+    var updated = function () {
         notifications.innerHTML = "キャッシュされました！変わったサイトを見るためにこのページをリフレッシュしてください。";
-        notifications.classList.toggle("finished");
-    }
-    cached = function(event) {
+        finish();
+    };
+    var cached = function () {
         notifications.innerHTML = "キャッシュされました！オフラインでもアクセス出来るようになりました。";
-        notifications.classList.toggle("finished");
-    }
+        finish();
+    };
     appcache.addEventListener("updateready", updated);
     appcache.addEventListener("cached", cached);
 });
