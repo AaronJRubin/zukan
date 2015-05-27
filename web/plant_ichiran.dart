5ca7f5208bc6f7126fbda8a75530825500c5987d
Aaron J Rubin <aaronjrubin1@gmail.com>
2015-05-28 02:37:43 +0900

    Beginning work on plant_ichiran.dart (not functional yet)

---
+++ web/plant_ichiran.dart
@@ -0,0 +1,82 @@
+import 'dart:html';
+import 'plant.dart';
+import 'article.dart';
+import 'ichiran_utils.dart';
+
+ElementList<CheckboxInputElement> includeCheckboxes;
+ElementList<CheckboxInputElement> excludeCheckboxes;
+TextInputElement articleSearch;
+ElementList<LIElement> plantTiles;
+
+void main() {
+  setUpSearchAreaToggle();
+  includeCheckboxes = document.querySelectorAll(".search-checkbox.include");
+  excludeCheckboxes = document.querySelectorAll(".search-checkbox.exclude");
+  articleSearch = document.querySelector("#article-search");
+  plantTiles = document.querySelectorAll("ol.tiles li");
+  /*for (int i = 0; i < includeCheckboxes.length; i++) {
+    CheckboxInputElement includeCheckbox = includeCheckboxes[i];
+    CheckboxInputElement excludeCheckbox = excludeCheckboxes[i];
+    includeCheckbox.onChange.listen((e) {
+      toggleCheckboxChanged(includeCheckbox, excludeCheckbox);
+    });
+    excludeCheckbox.onChange.listen((e) {
+      toggleCheckboxChanged(excludeCheckbox, includeCheckbox);
+    });
+  }*/
+  articleSearch.onChange.listen((e) => refresh());
+  articleSearch.onKeyUp.listen((e) => refresh());
+  refresh(); // maybe things were clicked before script was loaded
+}
+
+void toggleCheckboxChanged(CheckboxInputElement self, CheckboxInputElement partner) {
+  if (self.checked) {
+    if (partner.checked) {
+      partner.checked = false;
+    }
+  }
+  refresh();
+}
+
+Filter<Plant> buildFilter() {
+  Filter<Plant> filter = new Filter<Plant>();
+  String searchText = articleSearch.value.trim();
+  if (searchText.length > 0) {
+    filter
+        .add((plant) => article_map[plant.romaji].text.contains(searchText));
+  }
+  /*
+  for (CheckboxInputElement checkbox in includeCheckboxes) {
+    if (checkbox.checked) {
+      String area = checkbox.id.split("-")[1];
+      if (checkbox.id.startsWith("takatsu")) {
+        filter.add((plant) => plant.takatsu.contains(area));
+      } else if (checkbox.id.startsWith("masuda")) {
+        filter.add((plant) => plant.masuda.contains(area));
+      }
+    }
+  }
+  for (CheckboxInputElement checkbox in excludeCheckboxes) {
+    if (checkbox.checked) {
+      String area = checkbox.id.split("-")[1];
+      if (checkbox.id.startsWith("takatsu")) {
+        filter.add((plant) => !plant.takatsu.contains(area));
+      } else if (checkbox.id.startsWith("masuda")) {
+        filter.add((plant) => !plant.masuda.contains(area));
+      }
+    }
+  }*/
+  return filter;
+}
+
+void refresh() {
+  Filter filter = buildFilter();
+  for (LIElement tile in plantTiles) {
+    Plant plant = plant_map[tile.attributes["data-plant-name"]];
+    if (!filter.filter(plant)) {
+      tile.classes.add('hidden');
+    } else {
+      tile.classes.remove('hidden');
+    }
+  }
+}
