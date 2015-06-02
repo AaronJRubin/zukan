c6f8faa435cfda7899c95dbfd720300f25c1a6b3
Aaron J Rubin <aaronjrubin1@gmail.com>
2015-06-02 15:45:55 -0400

    Modify plant.py to not use features that we are not (currently) interested in

--- plant.py
+++ plant.py
@@ -5,20 +5,14 @@ import os
 
 class Plant:
 
-    def __init__(self, latin = "", ka = "", zoku = "", romaji = "", kana = "",
-            rarity = 3, display_name = "", saitei = 6, saikou = 10, iro = None):
+    def __init__(self, latin = "", romaji = "", kana = "",
+            display_name = ""):
         self.romaji = romaji
         try:
             self.kana = kana.decode('utf-8')
         except Exception:
-            self.kana = kana
-        self.iro = iro if iro else []
+            self.kana = kana 
         self.latin = latin if latin else ""
-        self.ka = ka if ka else ""
-        self.zoku = zoku if zoku else ""
-        self.rarity = rarity if rarity else 3
-        self.saitei = saitei if saitei else 6
-        self.saikou = saikou if saikou else 10
         self.display_name = display_name if display_name else self.kana
 
     def get_link(self):
@@ -26,6 +20,3 @@ class Plant:
 
     def image(self, suffix):
         return os.path.join("/images/shokubutsu", self.romaji, self.romaji + "-" + suffix + ".jpg")
-
-    def rarity_stars(self):
-        return ("&#x2605;" * self.rarity) + ("&#x2606;" * (5 - self.rarity)) 
