73163d390e071743b6eb8853d03a24468c74b0d4
Aaron J Rubin <aaronjrubin1@gmail.com>
2015-07-13 09:33:21 +0900

    Successfully parse new plant data

--- data/manually_processed/parse_plants.rb
+++ data/manually_processed/parse_plants.rb
@@ -25,7 +25,7 @@ TYPE_MAP = { "tanshiyou" => "単子葉植物", "soushiyou" => "双子葉植物",
 def parse_plant_file(plant_file)
   data = CSV.read(plant_file)[2..-1] # first two lines are headings
   type = TYPE_MAP[plant_file.gsub(".csv", "")] 
-  result = data.select { |row| not row[8].nil? } . map { |row|
+  result = data.select { |row| not row[0].nil? } . map { |row|
     res = {}
     row = row.map { |value| if value.nil? then value else value.strip.gsub("　", "").gsub("~", "〜") end }
     seiikubasho = []
@@ -60,11 +60,42 @@ def parse_plant_file(plant_file)
     # skip tokki for now
     res["latin"] = row[8]
     res["kana"] = row[9]
-    res["bunpu"] = if row[10].nil? then [] else row[10].gsub(/[()（） 　]/, "").chars.select { |char| char != "" } end
-    res["type"] = type
-    res
-  }  
-  return result
+		res["bunpu"] = if row[10].nil? then [] else row[10].gsub(/[()（） 　]/, "").chars.select { |char| char != "" } end
+		if type != "シダ類"
+			#puts "Parsing search parameters for plant of type #{type}"
+			res["hanotsukikata"] = row[11]
+			#puts "hananotsukikata = #{row[11]}"
+			res["hanokatachi"] = row[12]
+			#puts "hanokatachi = #{row[12]}"
+			res["kyoshi"] = row[13]
+			#puts "kyoshi = #{row[13]}"
+			res["hananotsukikata"] = row[14]
+			#puts "hananotsukikata = #{row[14]}"
+			petal_description = row[15] || ""
+			res["petal_description"] = petal_description
+			#puts "petal_description = #{row[15]}"
+			petal_counts = Set.new(petal_description.scan(/\d+/).map(&:to_i))
+			petal_ranges = petal_description.scan(/\d+〜\d+/)
+			petal_ranges.each do |petal_range|
+				puts "Parsing petal range: #{petal_range}"
+				beginning, ending = petal_range.split("〜").map(&:to_i)
+				((beginning + 1)...ending).each do |quantity|
+					petal_counts.add(quantity)
+				end
+			end
+			res["petal_counts"] = petal_counts.to_a.sort
+			#puts "petal_counts = #{res["petal_counts"]}"
+			res["color_description"] = row[16]
+			#puts "color_description = #{row[16]}"
+			#res["colors"] = row[16].split(/[・、～/
+			#puts "shokudoku = #{row[17]
+			shokudoku_description = row[17] || "不明"
+			res["shokudoku"] = shokudoku_description.gsub("・近縁種は毒", "").gsub("・沖縄「ンジャナ」", "").split("・")
+		end
+		res["type"] = type
+		res
+	}  
+	return result
 end
 
 result = Rake::FileList.new("*.csv").map { |file| parse_plant_file(file) } . flatten
@@ -72,11 +103,11 @@ result = Rake::FileList.new("*.csv").map { |file| parse_plant_file(file) } . fla
 result_map = {}
 
 result.each do |plant|
-  katakana = plant["kana"]
-  hiragana = Moji.kata_to_hira(katakana)
-  romaji = hiragana.to_roma.gsub("n'", "nn")
-  plant.delete("kana")
-  result_map[romaji] = plant
+	katakana = plant["kana"]
+	hiragana = Moji.kata_to_hira(katakana)
+	romaji = hiragana.to_roma.gsub("n'", "nn")
+	plant.delete("kana")
+	result_map[romaji] = plant
 end
 
 output_file = File.open("plant_data.yaml", "w")
